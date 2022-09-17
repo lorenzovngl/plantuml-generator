bb5f02e1c3a2c0e58b176837d33e312525adb758
Lorenzo Vainigli <lorenzovngl@gmail.com>
2022-09-17 18:14:51 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,26 @@
+#PlantUML Generator
+
+PlantUML Generator is a jar application to automatically generate [PlantUML](https://plantuml.com/) diagrams from source code.
+
+```
+Input folder
+|_ A.java
+|_ B.java
+|_ C.java
+|_ D.java
+|_ Main.java
+```
+
+![Example output](example.png)
+
+### How to use it
+
+The program needs two arguments:
+- the path of the folder containing the input files;
+- the path of the output file.
+
+Example:
+
+```
+java -jar PlantUMLGenerator.jar input output.puml
+```
---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.6.10"
+    application
+}
+
+version = "0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "MainKt"
+    }
+    configurations["compileClasspath"].forEach { file: File ->
+        from(zipTree(file.absoluteFile))
+    }
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
+}
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
